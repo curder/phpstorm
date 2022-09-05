156b8323cda0dfae7d19e2539a4daad1c44e0c4e
curder <q.curder@gmail.com>
2022-09-05 11:42:55 +0800

    Fase composition

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -51,6 +51,7 @@ function sidebarGuide() {
                 {text: "重构之向上拉成员", link: "/guide/refactoring-for-pull-members-up"},
                 {text: "重构之提取接口", link: "/guide/refactoring-for-extract-interface"},
                 {text: "重构之引入变量", link: "/guide/refactoring-for-introduce-variable"},
+                {text: "快速组合", link: "/guide/fast-composition"},
             ]
         }
     ];
