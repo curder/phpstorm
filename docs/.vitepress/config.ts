d8450416a4b983d8c2f95c783191af256d2f1c47
curder <q.curder@gmail.com>
2022-09-13 18:16:04 +0800

    Change Google Analytics code

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -26,12 +26,12 @@ export default defineConfig({
     head: [
         [
             'script',
-            { 'async': true, 'src': 'https://www.googletagmanager.com/gtag/js?id=G-5R2FD5K' }
+            { 'async': true, 'src': 'https://www.googletagmanager.com/gtag/js?id=G-S6RB4D9N5L' }
         ],
         [
             'script',
             {},
-            "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-5R2FD5K');"
+            "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-S6RB4D9N5L');"
         ]
     ],
 });
